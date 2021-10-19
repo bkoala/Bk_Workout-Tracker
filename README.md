ede6fa481c94124022a33c70be9ffc122a5cd76b
Ben Koala <bkoala123@hotmail.com>
2021-10-18 17:39:03 -0700

    Updated files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Bk_Workout-Tracker
+
+
+Install the required dependices to your cloned directory in command-line using: npm install
+
+Or install the packages individually...
+Install express to your cloned directory in command-line using: npm install express.
+Install mongoose to your cloned directory in command-line using: npm install mongoose
+Install morgan to your cloned directory in command-line using: npm install morgan
+Install nodemon to your cloned directory in command-line using: npm install --save-dev nodemon
--- server.js
+++ server.js
@@ -79,3 +79,51 @@ app.get("/populateduser", (req, res) => {
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+/*
+// BRING IN MONGOOSE DATABASE AND EXPRESS SERVER
+const express = require("express");
+const mongoose = require("mongoose");
+
+// VARIABLE FOR PORT
+const PORT = process.env.PORT || 3000
+
+// INITIATE INSTANCE OF EXPRESS SERVER AS YOUR APP
+const app = express();
+
+// DATA PARSING
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// PUBLIC DIRECTORY ACCESS
+app.use(express.static("public"));
+
+// ASYNC AWAIT FOR MONGOOSE DATABASE CONNECTION
+// CONSULTED CODE HERE: https://stackoverflow.com/questions/54890608/how-to-use-async-await-with-mongoose
+const connectDb = async () => {
+  await mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser:true,
+      useCreateIndex:true,
+      useFindAndModify:false,
+      useUnifiedTopology: true 
+    }
+  )
+  .then(() => {
+    console.log("Connected to database!")
+  })
+  .catch(() => {
+    console.log(err)
+  })
+}
+// CONNECT TO MONGOOSE DATABASE OR CATCH & CONSOLE LOG ERROR
+connectDb().catch(err => console.log(err))
+
+// ROUTES
+app.use(require("./routes/api.js"));
+
+// START LISTENING
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+*/
