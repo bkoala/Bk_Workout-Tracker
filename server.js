021488eade6716348d9a7eff7d0512fc12ebc4e8
Ben Koala <bkoala123@hotmail.com>
2021-10-18 19:46:36 -0700

    Updates to server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const apiRoutes=require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
 
 const app = express();
 
@@ -19,33 +18,6 @@ app.use(express.static("public"));
 app.use('/',apiRoutes);
 
 
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
-/*
-// BRING IN MONGOOSE DATABASE AND EXPRESS SERVER
-const express = require("express");
-const mongoose = require("mongoose");
-
-// VARIABLE FOR PORT
-const PORT = process.env.PORT || 3000
-
-// INITIATE INSTANCE OF EXPRESS SERVER AS YOUR APP
-const app = express();
-
-// DATA PARSING
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// PUBLIC DIRECTORY ACCESS
-app.use(express.static("public"));
-
-// ASYNC AWAIT FOR MONGOOSE DATABASE CONNECTION
-// CONSULTED CODE HERE: https://stackoverflow.com/questions/54890608/how-to-use-async-await-with-mongoose
 const connectDb = async () => {
   await mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
@@ -63,14 +35,9 @@ const connectDb = async () => {
     console.log(err)
   })
 }
-// CONNECT TO MONGOOSE DATABASE OR CATCH & CONSOLE LOG ERROR
+// Connect to the database
 connectDb().catch(err => console.log(err))
 
-// ROUTES
-app.use(require("./routes/api.js"));
-
-// START LISTENING
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-*/
