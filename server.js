b7433dfc278811b09340336ed6262e7bc03c6145
Ben Koala <bkoala123@hotmail.com>
2021-10-18 18:54:19 -0700

    Changes to server.js fiel

--- server.js
+++ server.js
@@ -11,12 +11,11 @@ const db = require("./models");
 
 const app = express();
 
-app.use(logger("dev"));
+//app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static("public"));
+app.use(express.static("public"));
 app.use('/',apiRoutes);
 app.use('/',htmlRoutes);
 
@@ -24,52 +23,6 @@ app.use('/',htmlRoutes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 
-
-  
- /* 
-app.get("/exercise", (req, res) => {
-  db.Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-
-app.get("/user", (req, res) => {
-  db.User.find({})
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.post("/submit", ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/populateduser", (req, res) => {
-  db.User.find({})
-    .populate("notes")
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-*/
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
