4f9240fa6a3d50a08f2cd9c221706feeca8adacc
Ben Koala <bkoala123@hotmail.com>
2021-10-18 19:01:58 -0700

    More Changes to server.js fiel

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use('/',apiRoutes);
+app.use('/api',apiRoutes);
 app.use('/',htmlRoutes);
 
 
